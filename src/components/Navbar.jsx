1660d9ff1cdd93e6e6a2ab0582d098e62aacf3d7
Chxndxn <chandangowda.kr2@gmail.com>
2024-03-01 12:45:34 +0530

    Added Social Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Logo from '../assets/logo.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -41,7 +43,39 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden'></div>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-tr-3xl hover:rounded-br-3xl'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] hover:rounded-r-3xl'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] hover:rounded-r-3xl'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded-br-3xl hover:rounded-tr-3xl'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
